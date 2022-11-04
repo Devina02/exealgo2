6129df7296beed43d3d8166d758e35e093cae726
Devina02 <devina.fitriapngs@gmail.com>
2022-11-04 15:50:38 +0700

    Membuat class

--- Program.cs
+++ Program.cs
@@ -5,4 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace exealgo2
-{ }
+{ 
+    class program
+    {
+        private int[] arr = new int[33];
+        private int n;
+        public void read()
+        {
+
+        }
+
+    }
+}
