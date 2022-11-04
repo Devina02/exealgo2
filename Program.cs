13eb996d03afd30168852ef732bd38aabdf57bae
Devina02 <devina.fitriapngs@gmail.com>
2022-11-04 16:27:51 +0700

    Membuat for untuk pengguna memasukan element pada array

--- Program.cs
+++ Program.cs
@@ -9,11 +9,32 @@ namespace exealgo2
     class program
     {
         private int[] arr = new int[33];
+        private int cmp_count = 0;
+        private int mov_count = 0;
         private int n;
-        public void read()
+        public void input()
         {
+            while (true)
+            {
+                Console.WriteLine("Masukan jumlah elemen atau data array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray maksimum 33 element atau data. \n");
+            }
+            Console.WriteLine("\n==================================");
+            Console.WriteLine("Enter array elementa");
+            Console.WriteLine("\n==================================");
 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+   
         }
-
     }
 }
